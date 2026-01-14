32fcee1d70a6c70d1f6a8609ffe14fa2d69f0815
aaronbinion20-dev <aaronbinion20@gmail.com>
2026-01-14 16:20:33 -0500

    Starter Code

---
+++ main/main.c
@@ -0,0 +1,47 @@
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+
+
+#define LED_PIN GPIO_NUM_5   // Choose your LED pin
+#define BUTTON_PIN GPIO_NUM_4   // Choose your button pin
+
+
+void app_main(void) {
+
+
+    gpio_config_t led_conf = {
+        .pin_bit_mask = (1ULL << LED_PIN),
+        .mode = GPIO_MODE_OUTPUT,          
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&led_conf);
+
+
+    gpio_config_t button_config = {
+        .pin_bit_mask = (1ULL << BUTTON_PIN),
+        .mode = GPIO_MODE_INPUT,          
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .intr_type = GPIO_INTR_DISABLE  
+    };
+    gpio_config(&button_config);
+
+    bool light = false;
+    bool state = false;
+
+    while (1) {
+      if ((gpio_get_level(BUTTON_PIN) == 0) && !state) {
+        state = true;
+        printf("1\n");
+      }
+      if (!(gpio_get_level(BUTTON_PIN) == 0) && state) {
+        gpio_set_level(LED_PIN, light);
+        light = !light;
+        state = false;
+        printf("2\n");
+      }
+      vTaskDelay(25 / portTICK_PERIOD_MS); //Use appropriate loop delays
+      }
+      }
