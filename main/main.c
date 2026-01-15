d842ec21b046246d56097d3cbd2942e3a06e229f
aaronbinion20-dev <aaronbinion20@gmail.com>
2026-01-15 12:41:50 -0500

    Added a new button

--- main/main.c
+++ main/main.c
@@ -9,7 +9,7 @@ and then turns it off when it is pressed and released again
 
 #define LED_PIN GPIO_NUM_5   // Choose your LED pin
 #define BUTTON_PIN GPIO_NUM_4   // Choose your button pin
-
+#define BUTTON_PIN2 GPIO_NUM_6
 
 void app_main(void) {
 
@@ -33,15 +33,24 @@ void app_main(void) {
     };
     gpio_config(&button_config);
 
+    gpio_config_t button_config2 = {
+        .pin_bit_mask = (1ULL << BUTTON_PIN2),
+        .mode = GPIO_MODE_INPUT,          
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .intr_type = GPIO_INTR_DISABLE  
+    };
+    gpio_config(&button_config2);
+
     bool light = false;
     bool state = false;
 
     while (1) {
-      if ((gpio_get_level(BUTTON_PIN) == 0) && !state) {
+      if ((gpio_get_level(BUTTON_PIN) == 0) && !state && (gpio_get_level(BUTTON_PIN2) == 1)) {
         state = true;
         printf("1\n");
       }
-      if (!(gpio_get_level(BUTTON_PIN) == 0) && state) {
+      if (!(gpio_get_level(BUTTON_PIN) == 0) && state && (gpio_get_level(BUTTON_PIN2) == 1)) {
         gpio_set_level(LED_PIN, light);
         light = !light;
         state = false;
